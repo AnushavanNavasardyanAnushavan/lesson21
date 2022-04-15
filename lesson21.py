5e9c0a0a17698d6fdabb281e76a460dfc3060f33
AnushavanNavasardyanAnushavan <navasardyan.anushavan@gmail.com>
2022-04-16 01:06:08 +0400

    algoritm

---
+++ lesson21.py
@@ -0,0 +1,59 @@
+# ---------------------------------Linear Search---------------
+# def leneary_cearch(my_list, n, x):
+#     for i in range(0, n):
+#         if my_list[i] == x:
+#             return i
+#     return -1
+# my_list = [2, 3, 4, 10, 40, 21]
+# x = int(input("enter number --> "))
+# n = len(my_list)
+# res = leneary_cearch(my_list, n, x)
+# if res == -1:
+#     print("ellement is not present in list")
+# else:
+#     print("ellement is  present at index ", res)
+# print(res)
+
+# ----------------------------------------------------------------
+# -------------------Binary Search----------------------------
+# def binary_search(my_list, n, start, stop):
+#     mid = (start + stop) // 2
+#     if n == my_list[mid]:
+#         return mid
+#     elif n < my_list[mid]:
+#         return binary_search(my_list, n, start, mid - 1)
+#     else:
+#         return binary_search(my_list, n, mid + 1, stop)
+        
+
+# my_list = [1, 2, 3, 5, 10, 15, 27, 34, 55, 78, 98]
+# n = int(input("ENTER NUMBER ---> "))
+# start = 0
+# stop =len(my_list)
+# print(binary_search(my_list, n, start, stop))
+
+
+
+
+# ---------------------------------Bubble Sort -------------------------
+
+# my_list = [10, 75, 43, 15, 25, -4, 27]
+
+# def bubbleSort(my_list):
+#     last_item = len(my_list) 
+#     for i in range(0, len(my_list)):
+#         for j in range(0, last_item - 1):
+#             if my_list[j] > my_list[j + 1]:
+#                 my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+#     return my_list
+# print("old list --> ", my_list)
+# print("new list --> ", bubbleSort(my_list))
+
+
+# ---------2-rd dzev-------------
+# my_list = [10, 75, 43, 15, 25, -4, 27, 8, 2]
+# for i in range(0, len(my_list) - 1):
+#     for j in range(0, len(my_list) - i - 1):
+#         if my_list[j] > my_list[j + 1]:
+#             my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+# print(my_list)
